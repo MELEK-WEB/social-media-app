30a2e3c08f36ae0f8bb2ddcae56021be4b1e4a79
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2024-01-27 15:50:14 +0100

    --gh pages

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter baseName="/social-media-app">
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
