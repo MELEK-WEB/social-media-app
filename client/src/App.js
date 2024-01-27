4eff9fa8b02f4cc849f99be44057fbefa1dda0b1
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2024-01-27 16:18:17 +0100

    gh fix routs

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/social-media-app/">
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
